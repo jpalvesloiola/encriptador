9c693055d460923316943db80bd49758655c3b6f
João Pedro Alves Loiola <39224574+jpalvesloiola@users.noreply.github.com>
2023-01-24 13:23:08 -0300

    Delete Encriptador directory

--- Encriptador/script.js
+++
@@ -1,8 +0,0 @@
-function autoResize()
-    {
-        objTextArea = document.getElementById('msg-criptografa');
-        while (objTextArea.scrollHeight > objTextArea.offsetHeight)
-        {
-            objTextArea.rows += 1;
-        }
-    }
